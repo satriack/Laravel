97bbf4d11412b1ecd1b99a9a0bb8b373583518c3
satriack <chandroided@gmail.com>
2018-05-25 11:11:00 +0700

    CR end at 35:40

--- resources/views/read.blade.php
+++ resources/views/read.blade.php
@@ -4,8 +4,7 @@
 @section('header') Read Page
 @endsection
 @section('content')
-<form action="/input" method="post">
-  {{ csrf_field() }}
+
   <a href="{{url('input')}}"> Tambah Data</a>
   <table border="1">
     <tr>
@@ -13,6 +12,7 @@
       <th>Nama</th>
       <th>Email</th>
       <th>Password</th>
+      <th colspan="2">Action</th>
     </tr>
     @foreach ($siswa as $data)
     <tr>
@@ -20,15 +20,23 @@
       <td>{{$data->name}}</td>
       <td>{{$data->email}}</td>
       <td>{{$data->password}}</td>
+      <td>
+        <a href="#">EDIT</a>
+
+      </td>
+      <td><form action="/delete/{{$data->id}}" method="POST">
+        {{ csrf_field() }}
+      <input type="submit" value="delete">
+      </form></td>
     </tr>
     @endforeach
     <tr>
-      <td colspan="4" style="text-align:right;">
+      <td colspan="5" style="text-align:right;">
         <input type="submit" value="Submit">
       </td>
     </tr>
   </table>
-</form>
+
 @endsection
 @section('namaview') readblade
 @endsection
